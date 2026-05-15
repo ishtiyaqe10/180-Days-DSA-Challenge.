7d2787d0883c46c0a13576162d594440cab95d9f
Ishtiayqe Ahmad <ishtiyaque2215@gmail.com>
2026-05-15 23:38:44 +0530

    Update Phase_1_Loops.cpp

--- Day-5/Phase_1_Loops.cpp
+++ Day-5/Phase_1_Loops.cpp
@@ -54,4 +54,79 @@ int n;
         cout << n << " * " << i << " = " << (n * i) << endl;
     }
     return 0;
+
+
+
+                    // Phase-2 Do-While Loops
+
+
+// Que  1. Print numbers from 1 to 10         
+
+for (int i = 1; i <= 10; i++) {
+        cout << i << " ";
+    }
+    return 0;
+
+
+// Que 2. Print the multiplication table of a given number.
+
+    int n;
+    cout << "Enter number: "; cin >> n;
+    for (int i = 1; i <= 10; i++) {
+        cout << n << " x " << i << " = " << n * i << endl;
+    }
+    return 0;
+
+
+// Que 3. Keep taking numbers from the user until 0 is entered, then print the sum of all entered numbers.
+
+int n, sum = 0;
+    do {
+        cout << "Enter a number (0 to stop): ";
+        cin >> n;
+        sum += n;
+    } while (n != 0);
+    cout << "Total Sum: " << sum;
+    return 0;
+
+
+// Que 4. Keep taking numbers from the user until 0 is entered, then print the largest number among all inputs.
+
+int n, maxNum = INT_MIN;
+    do {
+        cout << "Enter a number (0 to stop): ";
+        cin >> n;
+        if (n != 0 && n > maxNum) maxNum = n;
+    } while (n != 0);
+    cout << "Largest Number: " << maxNum;
+    return 0;    
+
+
+
+// Que 5. Count and print the number of digits in the given number.
+
+int n, count = 0;
+    cout << "Enter number: "; cin >> n;
+    if (n == 0) count = 1;
+    while (n != 0) {
+        n /= 10;
+        count++;
+    }
+    cout << "Number of digits: " << count;
+    return 0;
+}
+
+
+//  Que 6. Reverse the given number and print the reversed value.
+
+
+int n, rev = 0;
+    cout << "Enter number: "; cin >> n;
+    while (n > 0) {
+        rev = (rev * 10) + (n % 10);
+        n /= 10;
+    }
+    cout << "Reversed: " << rev;
+    return 0;
+  
 }
